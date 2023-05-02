51250e3208baff547733c97cc4040506af6d6e15
NickZRS <nick1804js@gmail.com>
2023-05-02 12:35:46 -0300

    commit02

--- src/app/pokedex.service.ts
+++ src/app/pokedex.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokedexService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+
+    this.carregarPokemons();
+  }
+  async carregarPokemons() {
+    const requisicao = await this.httpClient.get<any>('https://pokeapi.co/api/v2/pokemon/ditto').toPromise;
+
+    const pokemons = requisicao.results;
+    console.log(requisicao);
+  }
 }
---
+++ src/app/pokedex.ts
@@ -0,0 +1,7 @@
+export interface Pokedex {
+
+  name: string;
+  id: number;
+  height: number;
+  weight: number;
+}
